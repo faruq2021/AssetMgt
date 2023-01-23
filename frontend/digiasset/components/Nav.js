0be5f5dc0847b99d0d946daec34238e930ab2b3f
Dauda <umardauda@rocketmail.com>
2023-01-23 14:31:09 +0100

    Edited Nav and AuditAsset

--- frontend/digiasset/components/Nav.js
+++ frontend/digiasset/components/Nav.js
@@ -1,47 +1,53 @@
-import Link from 'next/link'
-import styled from 'styled-components'
-import logo from "../public/digiasset.png"
+import Link from "next/link";
+import styled from "styled-components";
+import logo from "../public/digiasset.png";
+import { Button } from "@chakra-ui/react";
 
 const NavContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
-  background-color: #0000FF;
+  background-color: #0000ff;
   color: #fff;
-`
+`;
 
 const NavLogo = styled.img`
   height: 20px;
-`
-const NavLinksContainer = styled.div` 
+`;
+const NavLinksContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
+`;
 const NavLink = styled.div`
   margin-left: 1rem;
   font-weight: bold;
-`
+`;
 
 const Nav = () => (
   <NavContainer>
     <Link href="/">
-    <NavLogo src={logo} alt="Logo" />
-      
+      <NavLogo src={logo} alt="Logo" />
     </Link>
     <NavLinksContainer>
       <Link href="/about">
-        <NavLink>About</NavLink>
+        <NavLink>
+          <Button colorScheme="blue">About</Button>
+        </NavLink>
       </Link>
       <Link href="/contact">
-        <NavLink>Contact</NavLink>
+        <NavLink>
+          <Button colorScheme="blue">Contact</Button>
+        </NavLink>
       </Link>
       <Link href="/auditAsset">
-        <NavLink>Audit Assets</NavLink>
+        <NavLink>
+          <Button colorScheme="blue">Audit Assets</Button>
+        </NavLink>
       </Link>
     </NavLinksContainer>
   </NavContainer>
-)
+);
 
 export default Nav;
--- frontend/digiasset/components/Welcome.js
+++ frontend/digiasset/components/Welcome.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const WelcomeText = styled.div`
   font-weight: bold;
